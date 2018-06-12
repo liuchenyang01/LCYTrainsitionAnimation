5c710abc86827135db9081c1028955db9f0489ba
liuchenyang <liuchenyang@changba.com>
2018-06-12 11:27:33 +0800

    Initial Commit

---
+++ LCYTrainsitionAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCYTrainsitionAnimation
+//
+//  Created by 刘晨阳 on 2018/6/12.
+//  Copyright © 2018年 changba-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCYTrainsitionAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCYTrainsitionAnimation
+//
+//  Created by 刘晨阳 on 2018/6/12.
+//  Copyright © 2018年 changba-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
